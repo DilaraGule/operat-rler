f9965f864bd127c9c1fbec0aec2f2169076dcdb6
DilaraGule <80154346+DilaraGule@users.noreply.github.com>
2022-02-15 14:49:01 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace operatörler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //atama ve işlemli atama
+            int x = 3;
+            int y = 2;
+
+            y = y + 2;
+            y += 2;
+            y /= 3;
+
+            x *= 2;
+            
+            //mantıksal operatörler
+            // ||, &&, !
+
+            //ilişkisel operatörler
+            // <, >, <=, >=, ==, !=
+
+            //aritmetik operatörler
+            // +, -, *, /
+
+            //% mod alır...
+        }
+    }
+}
